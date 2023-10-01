12045fcef5308fc1589dc95442b1c353f2d31644
Josiel Queiroz, JR <josielqueirozjr@icloud.com>
2023-10-01 17:34:39 -0300

    update app.js

--- todozorq/src/App.js
+++ todozorq/src/App.js
@@ -12,16 +12,47 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = (e) => { // Não vai quebrar o fluxo/sessão da página
+    e.preventDefault(); // ao enviar o formulário, chama-se fluxo SPA
+    setTitle(""); // Após o envio ele limpa o formulário
+  };
+
   return (
     <div className="App">
       <div className="todo-header">
         <h1>TODO ZORQ</h1>
       </div>
       <div className="form-todo">
-        <p>Formulário</p>
+        <h2>insira sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}> {/* Chamada da função do evento */}
+          <div className="form-control">
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input 
+            type="text"
+            name="title"
+            placeholder="Título da tarefa"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title || ""}
+            required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">O que você vai fazer?</label>
+            <input 
+            type="text"
+            name="time"
+            placeholder="Tempo estimado (horas)"
+            onChange={(e) => setTime(e.target.value)}
+            value={time || ""}
+            required
+            />
+          </div>
+          <input type="submit" value="Criar tarefa" />
+        </form>
       </div>
       <div className="list-todo">
-        <p>Lista</p>
+        <h2>lista de tarefas:</h2>
+        {tasks.length === 0 && <p>não há tarefas!</p>}
       </div>
     </div>
   );
