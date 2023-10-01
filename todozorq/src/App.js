24f9db4816a7cdc52ecc5e799502ea8d27cb7c71
Josiel Queiroz, JR <josielqueirozjr@icloud.com>
2023-10-01 17:10:34 -0300

    update css

--- todozorq/src/App.js
+++ todozorq/src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 
 import {useState, useEffect} from 'react'
+import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons/bs'
 
+const API = "http://localhost:5000";
 
 function App() {
+
+  const [title, setTitle] = useState(""); // estados para alterações das estruturas
+  const [time, setTime] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="App">
-      <h1>Todo ZORQ</h1>
+      <div className="todo-header">
+        <h1>TODO ZORQ</h1>
+      </div>
+      <div className="form-todo">
+        <p>Formulário</p>
+      </div>
+      <div className="list-todo">
+        <p>Lista</p>
+      </div>
     </div>
   );
 }
