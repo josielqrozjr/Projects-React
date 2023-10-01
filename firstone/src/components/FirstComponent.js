d66e5bcfcdf7b076714c8bec61366bfd50a1b2f2
Josiel Queiroz, JR <josielqueirozjr@icloud.com>
2023-10-01 14:25:09 -0300

    update components

---
+++ firstone/src/components/FirstComponent.js
@@ -0,0 +1,12 @@
+function FirstComponent() {
+
+    // Comentário no JS
+    return (
+    <div className="firstcomponent">
+        {/* Comentario no JSX */}
+        <p>Primeiro Componente</p>
+    </div>
+    )
+}
+
+export default FirstComponent
